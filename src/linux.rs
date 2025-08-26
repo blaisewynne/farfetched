78d9460b95d52ab09a7af01b4dcc6b3184fff4ea
installer <blaise.wynne1@gmail.com>
2025-08-26 20:51:55 +1000

    version 0.3.1

--- src/linux.rs
+++ src/linux.rs
@@ -1,4 +1,5 @@
 use std::process::{Command, Stdio};
+#[allow(unused)]
 pub fn main() {
     let os = get_os();
     let user = get_user();
@@ -8,6 +9,7 @@ pub fn main() {
     let kernel = get_kernel();
     let mem = get_ram();
     let memp = get_ram_percentage();
+    let battery = get_battery_percentage();
     print!("{}", os);
     print!("{}", user);
     print!("{} / {} (\x1b[32m{}\x1b[0m)\n", strg[3], strg[1], strg[4]);
@@ -15,6 +17,7 @@ pub fn main() {
     print!("{}", bashv);
     print!("{}", cpu);
     print!("{} / {} (\x1b[32m{}%\x1b[0m)\n", mem[2], mem[1], memp);
+    modify_battery();
 }
 
 fn get_os() -> String {
@@ -175,4 +178,62 @@ fn get_ram_percentage() -> i64 {
    let ram_percentage: f64 = memp_used / memp_total * 100.0;
    let ram_percentage: i64 = ram_percentage as i64;
    ram_percentage
+}
+
+fn get_battery_percentage() -> i64 {
+   let battery_cat_command = Command::new("cat")
+   .arg("/sys/class/power_supply/BAT0/capacity")
+   .output()
+   .expect("");
+   let output = String::from_utf8_lossy(&battery_cat_command.stdout);
+   let battery = output.to_string();
+   let battery: Vec<String> = battery.split_whitespace().map(str::to_string).collect();
+   let battery: i64 = battery[0].parse().unwrap();
+   battery
+}
+
+fn get_battery_status() -> String {
+    
+    let battery_cat_command = Command::new("cat")
+        .arg("/sys/class/power_supply/BAT0/uevent")
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let battery_grep_command = Command::new("grep")
+        .arg("POWER_SUPPLY_STATUS")
+        .stdin(Stdio::from(battery_cat_command.stdout.unwrap()))
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let battery_cut_command = Command::new("cut")
+        .arg("-d=")
+        .arg("-f2-")
+        .stdin(Stdio::from(battery_grep_command.stdout.unwrap()))
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let battery_tr_command = Command::new("tr")
+        .arg("-d")
+        .arg("\"")
+        .stdin(Stdio::from(battery_cut_command.stdout.unwrap()))
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let output = battery_tr_command.wait_with_output().unwrap();
+    let bstatus = String::from_utf8_lossy(&output.stdout);
+    let bstatus = bstatus.trim_end();
+    bstatus.to_string()
+}
+
+fn modify_battery() {
+   let battery = get_battery_percentage();
+   let batterystatus = get_battery_status();
+   if battery >= 50 {
+      print!("{}, (\x1b[32m{}%\x1b[0m)\n", batterystatus, battery);
+   } else if battery <= 40 {
+      print!("{}, (\x1b[31m{}%\x1b[0m)\n", batterystatus, battery);
+   } else {
+      print!("{}, (\x1b[33m{}%\x1b[0m)\n", batterystatus, battery);
+   }
+
 }
