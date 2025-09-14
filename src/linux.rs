2bfab10f5b299ecaf5845d6cadebfef8845216ac
installer <blaise.wynne1@gmail.com>
2025-09-14 16:46:49 +1000

    version 0.5.7

--- src/linux.rs
+++ src/linux.rs
@@ -62,10 +62,28 @@ fn get_desktop() {
    let desktop_environment = "XDG_CURRENT_DESKTOP";
 
    match env::var(desktop_environment).expect("").as_str() {
-      "Hyprland" => print!("DESKTOP: Hyprland\n"),
+      "Hyprland" => get_hyprland(),
       "KDE" => print!("DESKTOP: KDE Plasma\n"),
       _ => print!("Unknown Desktop")
    }
+}
+
+fn get_hyprland() {
+   let hyprland_command = Command::new("hyprctl")
+       .arg("version")
+       .stdout(Stdio::piped())
+       .spawn()
+       .unwrap();
+   let hyprland_sed = Command::new("sed")
+       .args(["-n", r"s/.*Tag:\(.*\),.*/\1/p"])
+       .stdin(Stdio::from(hyprland_command.stdout.unwrap()))
+       .stdout(Stdio::piped())
+       .spawn()
+       .unwrap();
+   let hyprland_output = hyprland_sed.wait_with_output().unwrap();
+   let hyprland = String::from_utf8_lossy(&hyprland_output.stdout);
+   print!("DESKTOP: Hyprland {}\n", hyprland.to_string().trim());
+
 }
 
 fn get_user_hostname() {
@@ -86,14 +104,14 @@ fn get_user_hostname() {
 }
 
 fn get_storage() {
-    let ps_child = Command::new("df")
+    let ps_command = Command::new("df")
         .arg("-h")
         .stdout(Stdio::piped())
         .spawn()                    
         .unwrap();                    
     let grep_child_one = Command::new("grep")
         .args(["-oP", "/dev/nvme0n1p2[^ ]* (.*)"])
-        .stdin(Stdio::from(ps_child.stdout.unwrap()))
+        .stdin(Stdio::from(ps_command.stdout.unwrap()))
         .stdout(Stdio::piped())
         .spawn()
         .unwrap();
